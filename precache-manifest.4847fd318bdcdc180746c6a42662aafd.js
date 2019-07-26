f08a03b171f725ee8d80feaef3258c1b749d5eba
Jungir <googlman6300@gmail.com>
2019-07-26 18:36:14 +0600

    todo app inital commit

---
+++ precache-manifest.4847fd318bdcdc180746c6a42662aafd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b06be7cf478a96a83b5f80ce61a79e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5464a65fd05d904390",
+    "url": "/static/css/main.487d8a70.chunk.css"
+  },
+  {
+    "revision": "89c17fe8733c46fbe8f9",
+    "url": "/static/js/2.78a64657.chunk.js"
+  },
+  {
+    "revision": "7f5464a65fd05d904390",
+    "url": "/static/js/main.b0314d72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
